ae00ea7ea20df3874248b783ee9cdcde3ad6a400
Frances7 <odohifunanyaj`gmail.com>
2023-03-28 21:25:10 +0100

    left sided tre

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -12,27 +12,20 @@
 binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 {
 	binary_tree_t *left_node = malloc(sizeof(binary_tree_t));
+	binary_tree_t *temp_node = malloc(sizeof(binary_tree_t));
+	binary_tree_t *new_node = malloc(sizeof(binary_tree_t));
 
-	if (parent == NULL)
+	temp_node = parent;
+	
+	if (temp_node == NULL)
 		return (NULL);
+	
 
-	parent->right = NULL;
-
-	if (parent->left != NULL)
-	{	
-		left_node->left = parent->left;
-		left_node = parent->left;
-		left_node->parent = parent;
-		left_node->n = value;
-		left_node->right = NULL;
-
-	}
-	else
-	{
-		left_node->parent = parent;
-		left_node->n = value;
-		left_node->right = NULL;
-	}
+	new_node = temp_node->left;
+	left_node->parent = temp_node;
+	left_node->n = value;
+	left_node->left = new_node;
+	temp_node->left = left_node;
 
 	return (left_node);
 }
--- 2-binary_tree_insert_right.c
+++ 2-binary_tree_insert_right.c
@@ -1,7 +1,7 @@
 #include "binary_trees.h"
 
 /**
- * binary_tree_insert_right - insert the left node of a binary tree
+ * binary_tree_insert_right - insert the right node of a binary tree
  *
  * @parent: The parent node of the node
  * @value: The value of the node
@@ -12,27 +12,19 @@
 binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
 {
 	binary_tree_t *right_node = malloc(sizeof(binary_tree_t));
+	binary_tree_t *temp = malloc(sizeof(binary_tree_t));
+	binary_tree_t *new_node = malloc(sizeof(binary_tree_t));
 
-	if (parent == NULL)
+	temp = parent;
+	
+	if (temp == NULL)
 		return (NULL);
-
-	parent->left = NULL;
-
-	if (parent->right != NULL)
-	{	
-		right_node->right = parent->right;
-		right_node = parent->right;
-		right_node->parent = parent;
-		right_node->n = value;
-		right_node->left = NULL;
-
-	}
-	else
-	{
-		right_node->parent = parent;
-		right_node->n = value;
-		right_node->left = NULL;
-	}
+	
+	new_node = temp->right;
+	right_node->parent = temp;
+	right_node->n = value;
+	right_node->right = new_node;
+	temp->right = right_node;
 
 	return (right_node);
 }
